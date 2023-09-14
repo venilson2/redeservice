c5da9bffa8350828e343cdfb5c9c3d9556b9685b
Venilson2 <venilson2009@hotmail.com>
2023-09-14 16:43:25 -0300

    ajustes gerais

--- redeservice/Controllers/HomeController.cs
+++ redeservice/Controllers/HomeController.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using redeservice.Models;
 using System.Diagnostics;
+using redeservice.Interfaces;
+using System.Text.Json;
 
 namespace redeservice.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IFileService _fileService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IFileService fileService)
         {
-            _logger = logger;
+            _fileService = fileService;
         }
 
         public IActionResult Index()
@@ -21,6 +23,59 @@ namespace redeservice.Controllers
         public IActionResult NumberOrdering()
         {
             return View();
+        }
+
+        public IActionResult DataJson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveNumbersFile(List<int> numbers)
+        {
+            try
+            {
+                string numbersText = string.Join(Environment.NewLine, numbers);
+
+                if (_fileService.SaveFile("numeros_ordenados.txt", "Files", numbersText))
+                {
+                    return Json(new { success = true });
+                }
+                else
+                {
+                    return Json(new { success = false, error = "Falha ao salvar o arquivo." });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, error = ex.Message });
+            }
+        }
+
+
+        public ActionResult GenerateDataJson()
+        {
+            List<ClsTeste> clsTestes = new();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                clsTestes.Add(new ClsTeste
+                {
+                    codigo = i,
+                    descricao = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff")
+                });
+            }
+
+            string jsonData = JsonSerializer.Serialize(clsTestes);
+
+            if (_fileService.SaveFile("data.json", "Files", jsonData))
+            {
+                return Json(new { success = true });
+            }
+            else
+            {
+                return Json(new { success = false, error = "Falha ao gerar arquivo JSON." });
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ redeservice/Models/ClsTeste.cs
@@ -0,0 +1,8 @@
+﻿namespace redeservice.Models
+{
+    public class ClsTeste
+    {
+        public int codigo { get; set; }
+        public string? descricao { get; set; }
+    }
+}
